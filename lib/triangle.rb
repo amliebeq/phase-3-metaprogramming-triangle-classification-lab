f7132efb54ac72920de35bb08021e103b359206b
alex lieberman <alex.lieberman182@gmail.com>
2023-04-17 08:33:47 -0700

    commit tree 39c054027d3e7d6cbaa27998614a2070bed7665e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :l1, :l2, :l3
+  
+  def initialize (l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+  
+  def kind
+    if @l1 + @l2 + @l3 == 0
+    raise TriangleError 
+    elsif @l1 < 0 || @l2 < 0 || @l3 < 0
+      raise TriangleError
+    elsif @l1 + @l2 <= @l3 || @l2 + @l3 <= @l1 || @l3 + @l1 <= @l2
+      raise TriangleError           
+    elsif l1 == l2 && l2 == l3 && l1 + l2 + l3 > 0
+      :equilateral
+    elsif l1 == l2 || l2 == l3 || l1 == l3
+      :isosceles
+    elsif l1 != l2 && l2 != l3 && l1 != l3
+      :scalene
+    end  
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Your triangle is invalid"
+    end  
+  end 
 end
